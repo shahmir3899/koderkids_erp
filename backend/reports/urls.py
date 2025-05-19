92b5d57e62b3654a95ad529bcf889a984043c0e5
shahmir3899 <shahmir3899@gmail.com>
2025-05-19 13:57:19 +0500

    urls mapped correctly

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -4,5 +4,5 @@ app_name = 'reports'
 urlpatterns = [
     path('student-report-data/', student_report_data, name='student_report_data'),
     path('generate-pdf/', generate_pdf, name='generate_pdf'),
-    path('api/generate-pdf-batch/', generate_pdf_batch, name='generate-pdf-batch'),
+    path('generate-pdf-batch/', generate_pdf_batch, name='generate-pdf-batch'),
 ]
