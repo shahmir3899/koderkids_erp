0a6bc3457697d0e45bbd26e21f391ff4083ecf04
shahmir3899 <shahmir3899@gmail.com>
2025-05-19 18:18:35 +0500

    bugs

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import generate_pdf, student_report_data, generate_pdf_batch
-
+from .views import generate_pdf, student_report_data
 app_name = 'reports'
 urlpatterns = [
     path('student-report-data/', student_report_data, name='student_report_data'),
